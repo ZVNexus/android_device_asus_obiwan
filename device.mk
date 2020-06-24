07c202b86d7372cd23ad8f17a6a35a448348c2d8
P.Adarsh Reddy <padarshr@codeaurora.org>
2020-06-24 20:41:51 +0530

    obiwan: Enable incremental FS feature.
    Change-Id: I23e19b4fd6f65d9abeabb50b7b417db966717ccd

--- device.mk
+++ device.mk
@@ -103,6 +103,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl-qti \
     android.hardware.health@2.1-service
 
+# Incremental FS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.incremental.enable=1
+
 # Init
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
