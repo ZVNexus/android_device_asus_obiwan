d8e11ff43eed3754ceef2f54984c0cffa7e7c6aa
Alexander Koskovich <zvnexus@outlook.com>
2021-03-20 19:22:40 -0700

    obiwan: Add ROG specific VINTF entries.
    Change-Id: Ie6c824478d36531a23c9fc5171f9b4798aa6c24c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,10 +67,12 @@ TARGET_SCREEN_DENSITY := 420
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/rog_vendor_framework_compatibility_matrix.xml \
     vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE += \
-    $(DEVICE_PATH)/manifest.xml
+    $(DEVICE_PATH)/manifest.xml \
+    $(DEVICE_PATH)/rog_manifest.xml
 
 DEVICE_MATRIX_FILE += \
     device/qcom/common/compatibility_matrix.xml
